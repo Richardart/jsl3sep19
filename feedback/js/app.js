a998f8dfda9d14e87687212f220ca6795b3596d3
Tun Lin Aung <mctun.kyawl0vie@gmail.com>
2021-09-19 19:12:28 +0630

    Sep 19 2021

---
+++ feedback/js/app.js
@@ -0,0 +1,15 @@
+// UI
+const panel = document.querySelector('#panel');
+const ratingcontainer = document.querySelector('.ratings-container');
+const ratings = document.querySelectorAll('.rating');
+const sendbtn = document.querySelector('#send');
+
+// bubbling
+panel.addEventListener('click',(e)=>{
+   // console.log('click');
+   // console.log(e.target);
+
+});
+
+
+// 19FB WDF......
